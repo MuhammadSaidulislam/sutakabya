9e3238c1e376f06117a60b100ef12bf147ebac8e
MuhammadSaidulislam <mislam171222@bscse.uiu.ac.bd>
2026-09-17 09:08:15 +0600

    db pool change

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,3 @@
-// import mysql from "mysql2/promise";
-
-// export const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
 import { Pool } from "pg";
 
 const databaseUrl = process.env.DATABASE_URL;
@@ -19,14 +6,26 @@ if (!databaseUrl) {
   throw new Error("DATABASE_URL is not configured");
 }
 
-const db = new Pool({
-  connectionString: databaseUrl,
-  max: 5,
-  idleTimeoutMillis: 10000,
-  connectionTimeoutMillis: 10000,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+declare global {
+  // eslint-disable-next-line no-var
+  var pgPool: Pool | undefined;
+}
+
+const db =
+  global.pgPool ??
+  new Pool({
+    connectionString: databaseUrl,
+    max: 3,
+    min: 0,
+    idleTimeoutMillis: 10000,
+    connectionTimeoutMillis: 10000,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.pgPool = db;
+}
 
 export default db;
